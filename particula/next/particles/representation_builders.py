6aeec0bf06dc0cc38070839347786afe1f0a5290
Kyle Gorkowski <kkgorkow@gmail.com>
2024-06-02 16:07:34 -0600

    Particle Representation Strategy, and docs settings Fixes Gorkowski/particula#9

---
+++ particula/next/particles/representation_builders.py
@@ -0,0 +1,24 @@
+"""
+This module contains the builders for the particle representation classes.
+
+Classes:
+- HistogramParticleRepresentationBuilder: Builder class for
+    HistogramParticleRepresentation objects.
+- PDFParticleRepresentationBuilder: Builder class for PDFParticleRepresentation
+    objects.
+- DiscreteParticleRepresentationBuilder: Builder class for
+    DiscreteParticleRepresentation objects.
+"""
+
+from typing import Dict, Any, Union
+import logging
+from numpy.typing import NDArray
+
+from particula.next.abc_builder import (
+    BuilderABC,
+    BuilderMolarMassMixin,
+    BuilderDensityMixin,
+    BuilderSurfaceTensionMixin,
+    BuilderSurfaceStrategyMixin,
+)
+from particula.next.particles.representation import ParticleRepresentation
