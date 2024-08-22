7d10de9cc5aad7a6bc45aad96953672d93c45d31
Kyle Gorkowski <kkgorkow@gmail.com>
2024-08-22 12:39:40 -0600

    Incorporate aerodynamic corrections added test Fixes Gorkowski/particula#29

--- docs/next/Tutorials/aerodynamic_convert_example.py
+++ docs/next/Tutorials/aerodynamic_convert_example.py
@@ -20,15 +20,30 @@ particle_radius = np.logspace(-7, -5, 100)  # m
 
 # %% derived properties
 
-mean_free_path_air = mean_free_path.molecule_mean_free_path(
-    temperature=temperature, pressure=pressure
-)
-
 
 # Function to calculate aerodynamic length error
 def cost_aerodynamic_length(
-    guess_aerodynamic_length, mean_free_path_air, particle_radius
+    guess_aerodynamic_length,
+    mean_free_path_air,
+    particle_radius,
+    **kwargs
 ):
+    """
+    Optimization cost function to find the aerodynamic length of a particle.
+
+    Args:
+        guess_aerodynamic_length (float): The initial guess for the aerodynamic length.
+        mean_free_path_air (float): The mean free path of air.
+        particle_radius (float): The radius of the particle.
+        kwargs : Additional keyword arguments for the optimization function.
+        "density" (float): The density of the particle.
+        "reference_density" (float): The reference density for the aerodynamic length.
+        "aerodynamic_shape_factor" (float): The aerodynamic shape factor.
+
+    Returns:
+        The error between the guessed aerodynamic length and the calculated aerodynamic length.
+
+    """
     physical_knudsen_number = properties.calculate_knudsen_number(
         mean_free_path_air, particle_radius
     )
@@ -47,9 +62,9 @@ def cost_aerodynamic_length(
         physical_length=particle_radius,
         physical_slip_correction_factor=physical_slip_correction,
         aerodynamic_slip_correction_factor=guess_aerodynamic_slip_correction,
-        density=2000,
-        reference_density=1000,
-        aerodynamic_shape_factor=1.3,
+        density=kwargs.get("density", 1500),
+        reference_density=kwargs.get("reference_density", 1000),
+        aerodynamic_shape_factor=kwargs.get("aerodynamic_shape_factor", 1),
     )
 
     # Error between guessed aerodynamic length and the guess for aerodynamic length
@@ -57,8 +72,9 @@ def cost_aerodynamic_length(
 
 
 def cost_physical_length(
-    guess_phsycial_length, mean_free_path_air, aerodynamic_length
+    guess_phsycial_length, mean_free_path_air, aerodynamic_length, **kwargs
 ):
+
     guess_physical_knudsen_number = properties.calculate_knudsen_number(
         mean_free_path_air, guess_phsycial_length
     )
@@ -77,13 +93,72 @@ def cost_physical_length(
         physical_length=guess_phsycial_length,
         physical_slip_correction_factor=guess_physical_slip_correction,
         aerodynamic_slip_correction_factor=aerodynamic_slip_correction,
-        density=2000,
-        reference_density=1000,
-        aerodynamic_shape_factor=1.3,
+        density=kwargs.get("density", 1500),
+        reference_density=kwargs.get("reference_density", 1000),
+        aerodynamic_shape_factor=kwargs.get("aerodynamic_shape_factor", 1),
     )
 
     # Error between guessed aerodynamic length and the guess for aerodynamic length
     return (new_aerodynamic_length - aerodynamic_length) ** 2
+
+# functions
+
+def convert_aerodynamic_to_physical_length(
+    aerodynamic_length,
+    pressure,
+    temperature,
+    particle_density,
+    aerodynamic_shape_factor=1,
+    reference_density=1000,
+) -> np.ndarray:
+    
+    mean_free_path_air = mean_free_path.molecule_mean_free_path(
+        temperature=temperature, pressure=pressure
+    )
+    keywords = {
+        "density": particle_density,
+        "aerodynamic_shape_factor": aerodynamic_shape_factor,
+        "reference_density": reference_density,
+    }
+    initial_physical_guess = aerodynamic_length
+
+    # solve for physical length
+    optimal_physical_length = fsolve(
+        cost_physical_length,
+        initial_physical_guess,
+        args=(mean_free_path_air, aerodynamic_length, keywords),
+    )
+
+    return optimal_physical_length
+
+
+def convert_physical_to_aerodynamic_length(
+    physical_length,
+    pressure,
+    temperature,
+    particle_density,
+    aerodynamic_shape_factor=1,
+    reference_density=1000,
+) -> np.ndarray:
+    
+    mean_free_path_air = mean_free_path.molecule_mean_free_path(
+        temperature=temperature, pressure=pressure
+    )
+    keywords = {
+        "density": particle_density,
+        "aerodynamic_shape_factor": aerodynamic_shape_factor,
+        "reference_density": reference_density,
+    }
+    initial_guess = physical_length
+
+    # solve for aerodynamic length
+    optimal_aerodynamic_length = fsolve(
+        cost_aerodynamic_length,
+        initial_guess,
+        args=(mean_free_path_air, physical_length, keywords),
+    )
+
+    return optimal_aerodynamic_length
 
 
 # %% aerodynamic size root finding
