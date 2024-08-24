c3cd21144ba2ec2cc7f646f45aad70ed2a60e478
Kyle Gorkowski <kkgorkow@gmail.com>
2024-08-23 19:00:26 -0600

    drafted sdm version

--- docs/next/Tutorials/coagulation_SDM_example.py
+++ docs/next/Tutorials/coagulation_SDM_example.py
@@ -29,7 +29,7 @@ from particula.util.convert import distribution_convert_pdf_pms
 # %% sample lognormal distribution
 
 radius_bins = np.logspace(
-    -9, -6, num=10
+    -9, -6, num=20
 )  # Define the radius bins for the distribution
 mass_bins = (
     4 / 3 * np.pi * radius_bins**3 * 1000
@@ -55,7 +55,9 @@ particle_radius = lognormal_sample_distribution(
 particles_original = particle_radius.copy()
 
 fig, ax = plt.subplots()
-ax.hist(particle_radius, bins=100, histtype="step", color="black", density=True)
+ax.hist(
+    particle_radius, bins=100, histtype="step", color="black", density=True
+)
 ax.set_xscale("log")
 ax.set_yscale("log")
 ax.set_xlabel("Particle radius (m)")
@@ -68,11 +70,14 @@ plt.show()
 # overflow test for bins
 # particle_radius[0] = 5e-10
 # particle_radius[-1] = 5e-6
-random_concentration = np.random.randint(1, 2, size=particle_radius.size)
-particle_concentration = np.ones_like(particle_radius) * random_concentration * 1e6  # particles per m^3
+random_concentration = np.random.randint(1, 10, size=particle_radius.size)
+particle_concentration = (
+    np.ones_like(particle_radius) * random_concentration * 1e4
+)  # particles per m^3
 volume_sim = 1  # m^3
 particle_radius = np.sort(particle_radius)
 number_in_bins, bins = np.histogram(particle_radius, bins=radius_bins)
+print(f"Total number of particles: {np.sum(particle_concentration)}")
 
 # Get the bin index for each particle (0-based indexing)
 bin_indices = np.digitize(particle_radius, bins, right=True)
@@ -93,57 +98,96 @@ concentration_in_bins = np.zeros_like(number_in_bins)
 for unique_bin in unique_bins:
     concentration_in_bins[unique_bin] = np.sum(
         particle_concentration[bin_indices == unique_bin]
-        )
+    )
 concentration_in_bins.astype(np.float64)
 
 
 delta_t = 1  # time step in seconds
-# total_number_concentration = 10000 * 1e6  # particles per m^3
-# total_number_tracked = np.sum(number_in_bins)
-# volume_sim = total_number_tracked / total_number_concentration
 
 # Initialize spline for kernel interpolation
 interp_kernel = RectBivariateSpline(
     x=radius_bins, y=radius_bins, z=kernel_bins
 )
+# Initialize spline for kernel interpolation
+interp_kernel = RectBivariateSpline(
+    x=radius_bins, y=radius_bins, z=kernel_bins
+)
 
+
+# def kernel_nearest_neighbor_extrapolation(
+#     interp_func, x_vals, y_vals, x_bins, y_bins
+# ):
+#     # Clamping x_vals to be within the bounds of x_bins
+#     x_vals_clamped = np.clip(x_vals, x_bins[0], x_bins[-1])
+
+#     # Clamping y_vals to be within the bounds of y_bins
+#     y_vals_clamped = np.clip(y_vals, y_bins[0], y_bins[-1])
+
+#     # Use the interpolating function with clamped values
+#     return interp_func.ev(x_vals_clamped, y_vals_clamped)
+
+
+# # Use nearest_neighbor_extrapolation instead of interp_kernel.ev directly
+# B_values = nearest_neighbor_extrapolation(
+#     interp_kernel,
+#     particle_radius[indices_i],
+#     particle_radius[indices_j],
+#     radius_bins,
+#     radius_bins,
+# )
 loss = np.zeros_like(particle_radius)
 gain = np.zeros_like(particle_radius)
-
+single_event_counter = np.zeros_like(particle_radius)
 
 # pair_indices.pop(-1)  # Remove the last pair (k, k)
 
 # Vectorized operations over bin pairs
 for k, l in pair_indices:
     # Kernel value at the max size for the pair of bins
-    Kmax = kernel_bins[k, l+1]
+    Kmax = kernel_bins[k, l + 1]
 
     # Calculate the number of particle pairs
     if k != l:
         N_pairs = (
-            number_in_bins[k] * number_in_bins[l]
-            * concentration_in_bins[k] * concentration_in_bins[l]
-            )
+            Kmax
+            * number_in_bins[k]
+            * number_in_bins[l]
+            * concentration_in_bins[k]
+            * concentration_in_bins[l]
+        )
     else:
         N_pairs = (
-            0.5 * number_in_bins[k] * (number_in_bins[l] - 1)
-            * concentration_in_bins[k] * (concentration_in_bins[l])
-            )
+            Kmax
+            * 0.5
+            * number_in_bins[k]
+            * (number_in_bins[l] - 1)
+            * concentration_in_bins[k]
+            * (concentration_in_bins[l])
+        )
 
     # Determine the exact number of events and sample from a Poisson distribution
-    N_events_exact = N_pairs * Kmax / volume_sim
+    N_events_exact = N_pairs / volume_sim
     N_events = rng.poisson(N_events_exact * delta_t)
 
     # Skip iteration if no events are expected
     if N_events == 0:
         continue
 
-    # Vectorized random particle selection
+    # Vectorized random particle selection, may remove if always max N_events
+    N_events = number_in_bins[k] if N_events > number_in_bins[k] else N_events
+    N_events = number_in_bins[l] if N_events > number_in_bins[l] else N_events
+
     r_i_indices = np.random.randint(
-        0, number_in_bins[k], size=N_events, dtype=int,
+        0,
+        number_in_bins[k],
+        size=N_events,
+        dtype=int,
     )
     r_j_indices = np.random.randint(
-        0, number_in_bins[l], size=N_events, dtype=int,
+        0,
+        number_in_bins[l],
+        size=N_events,
+        dtype=int,
     )
 
     # Get the indices in the particle array where the bins start
@@ -155,8 +199,11 @@ for k, l in pair_indices:
     indices_j = start_index_l + r_j_indices
 
     # Early exit for zero-sized particles due to previous coagulation
-    valid_indices = (particle_radius[indices_i] > 0) & (
-        particle_radius[indices_j] > 0
+    valid_indices = (
+        (particle_radius[indices_i] > 0)
+        & (particle_radius[indices_j] > 0)
+        & (single_event_counter[indices_i] < 1)
+        & (single_event_counter[indices_j] < 1)
     )
     if not np.any(valid_indices):
         continue
@@ -172,6 +219,7 @@ for k, l in pair_indices:
 
     # Perform coagulation with probability B/Kmax
     coagulation_probabilities = B_values / Kmax
+    print(coagulation_probabilities)
     coagulation_events = (
         rng.random(len(coagulation_probabilities)) < coagulation_probabilities
     )
@@ -182,27 +230,69 @@ for k, l in pair_indices:
 
     # Calculate new radii for coagulation events
     new_radii = (
-        particle_radius[indices_i] ** 3
-        + particle_radius[indices_j] ** 3
+        particle_radius[indices_i] ** 3 + particle_radius[indices_j] ** 3
     ) ** (1 / 3)
-
-    # Concentration distribution
-
 
     # Save radii gain and loss for coagulation events
     loss[indices_i] = particle_radius[indices_i]
     loss[indices_j] = particle_radius[indices_j]
     gain[indices_j] = new_radii
 
-    # Update the particle array
-    particle_radius[indices_i] = 0
-    particle_radius[indices_j] = new_radii
+    # Concentration change for coagulation events
+    concentration_delta = (
+        particle_concentration[indices_i] - particle_concentration[indices_j]
+    )
+    small_particle_concentration = concentration_delta > 0
+    large_particle_concentration = concentration_delta < 0
+    split_concentration = concentration_delta == 0
 
+    # equal number of large and small particles
+    if np.any(split_concentration):
+        # Split the concentration for equal mass
+        particle_concentration[indices_i[split_concentration]] /= 2
+        particle_concentration[indices_j[split_concentration]] /= 2
+        # Update the particle array
+        particle_radius[indices_i[split_concentration]] = new_radii[
+            split_concentration
+        ]
+        particle_radius[indices_j[split_concentration]] = new_radii[
+            split_concentration
+        ]
+        # keep track of the number of events per particle
+        single_event_counter[indices_i[split_concentration]] += 1
+        single_event_counter[indices_j[split_concentration]] += 1
+
+    # more large particles than small particles
+    if np.any(large_particle_concentration):
+        # remove the concentration from large particles, small concentration stays the same
+        particle_concentration[indices_j[large_particle_concentration]] = (
+            np.abs(concentration_delta[large_particle_concentration])
+        )
+        # Update the particle array, small concentration grow in size
+        particle_radius[indices_i[large_particle_concentration]] = new_radii[large_particle_concentration]
+        # indices j stay the same
+
+    # more small particles than large particles
+    if np.any(small_particle_concentration):
+        # remove the concentration from small particles, large concentration stays the same
+        particle_concentration[indices_i[small_particle_concentration]] = (
+            np.abs(concentration_delta[small_particle_concentration])
+        )
+        # Update the particle array, large concentration grow in size
+        particle_radius[indices_j[small_particle_concentration]] = new_radii[small_particle_concentration]
+        # indices i stay the same
+
+#
+print(f"Final number of particles: {np.sum(particle_concentration)}")
 
 # %% plot new distribution
 fig, ax = plt.subplots()
-ax.hist(particles_original, bins=100, histtype="step", color="blue", density=True)
-ax.hist(particle_radius, bins=100, histtype="step", color="black", density=True)
+ax.hist(
+    particles_original, bins=100, histtype="step", color="blue", density=True
+)
+ax.hist(
+    particle_radius, bins=100, histtype="step", color="black", density=True
+)
 ax.set_xscale("log")
 ax.set_yscale("log")
 ax.set_xlabel("Particle radius (m)")
