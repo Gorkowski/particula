dac62ee46a9d2bb7cfe21456be96828b2ae19d11
Kyle Gorkowski <kkgorkow@gmail.com>
2024-08-30 11:10:45 -0600

    update wall loss test a layout dynamics

--- particula/next/particles/properties/diffusion_coefficient.py
+++ particula/next/particles/properties/diffusion_coefficient.py
@@ -5,6 +5,21 @@ import numpy as np
 from numpy.typing import NDArray
 
 from particula.constants import BOLTZMANN_CONSTANT
+from particula.next.gas.properties.dynamic_viscosity import (
+    get_dynamic_viscosity
+)
+from particula.next.gas.properties.mean_free_path import (
+    molecule_mean_free_path
+)
+from particula.next.particles.properties.aerodynamic_mobility_module import (
+    particle_aerodynamic_mobility
+)
+from particula.next.particles.properties.slip_correction_module import (
+    cunningham_slip_correction
+)
+from particula.next.particles.properties.knudsen_number_module import (
+    calculate_knudsen_number
+)
 
 
 def particle_diffusion_coefficient(
@@ -28,4 +43,56 @@ def particle_diffusion_coefficient(
     """
     return (
         boltzmann_constant * temperature * particle_aerodynamic_mobility
+    )
+
+
+def particle_diffusion_coefficient_via_system_state(
+    particle_radius: Union[float, NDArray[np.float64]],
+    temperature: float,
+    pressure: float,
+) -> Union[float, NDArray[np.float64]]:
+    """
+    Calculate the diffusion coefficient of a particle.
+
+    Arguments:
+        temperature: The temperature of the system in Kelvin (K).
+        particle_radius: The radius of the particle in meters (m).
+        pressure: The pressure of the system in Pascals (Pa).
+
+    Returns:
+        The diffusion coefficient of the particle in square meters per
+        second (m²/s).
+    """
+
+    # Step 1: Calculate the dynamic viscosity of the gas
+    dynamic_viscosity = get_dynamic_viscosity(temperature=temperature)
+
+    # Step 2: Calculate the mean free path of the gas molecules
+    mean_free_path = molecule_mean_free_path(
+        temperature=temperature,
+        pressure=pressure,
+        dynamic_viscosity=dynamic_viscosity,
+    )
+
+    # Step 3: Calculate the Knudsen number (characterizes flow regime)
+    knudsen_number = calculate_knudsen_number(
+        mean_free_path=mean_free_path, particle_radius=particle_radius
+    )
+
+    # Step 4: Calculate the slip correction factor (Cunningham correction)
+    slip_correction_factor = cunningham_slip_correction(
+        knudsen_number=knudsen_number,
+    )
+
+    # Step 5: Calculate the particle aerodynamic mobility
+    aerodynamic_mobility = particle_aerodynamic_mobility(
+        radius=particle_radius,
+        slip_correction_factor=slip_correction_factor,
+        dynamic_viscosity=dynamic_viscosity,
+    )
+
+    # Step 6: Calculate the particle diffusion coefficient
+    return particle_diffusion_coefficient(
+        temperature=temperature,
+        particle_aerodynamic_mobility=aerodynamic_mobility,
     )
--- particula/next/particles/properties/settling_velocity.py
+++ particula/next/particles/properties/settling_velocity.py
@@ -5,6 +5,18 @@ import numpy as np
 from numpy.typing import NDArray
 
 from particula.constants import STANDARD_GRAVITY
+from particula.next.gas.properties.dynamic_viscosity import (
+    get_dynamic_viscosity,
+)
+from particula.next.gas.properties.mean_free_path import (
+    molecule_mean_free_path,
+)
+from particula.next.particles.properties.slip_correction_module import (
+    cunningham_slip_correction,
+)
+from particula.next.particles.properties.knudsen_number_module import (
+    calculate_knudsen_number,
+)
 
 
 def particle_settling_velocity(
@@ -41,3 +53,51 @@ def particle_settling_velocity(
     )
 
     return settling_velocity
+
+
+def particle_settling_velocity_via_system_state(
+    particle_radius: Union[float, NDArray[np.float64]],
+    particle_density: Union[float, NDArray[np.float64]],
+    temperature: float,
+    pressure: float,
+) -> Union[float, NDArray[np.float64]]:
+    """
+    Calculate the settling velocity of a particle.
+
+    Arguments:
+        particle_radius: The radius of the particle in meters (m).
+        particle_density: The density of the particle (kg/m³).
+        temperature: The temperature of the system in Kelvin (K).
+        pressure: The pressure of the system in Pascals (Pa).
+
+    Returns:
+        The settling velocity of the particle in meters per second (m/s).
+    """
+
+    # Step 1: Calculate the dynamic viscosity of the gas
+    dynamic_viscosity = get_dynamic_viscosity(temperature=temperature)
+
+    # Step 2: Calculate the mean free path of the gas molecules
+    mean_free_path = molecule_mean_free_path(
+        temperature=temperature,
+        pressure=pressure,
+        dynamic_viscosity=dynamic_viscosity,
+    )
+
+    # Step 3: Calculate the Knudsen number (characterizes flow regime)
+    knudsen_number = calculate_knudsen_number(
+        mean_free_path=mean_free_path, particle_radius=particle_radius
+    )
+
+    # Step 4: Calculate the slip correction factor (Cunningham correction)
+    slip_correction_factor = cunningham_slip_correction(
+        knudsen_number=knudsen_number,
+    )
+
+    # Step 5: Calculate the particle settling velocity
+    return particle_settling_velocity(
+        particle_radius=particle_radius,
+        particle_density=particle_density,
+        slip_correction_factor=slip_correction_factor,
+        dynamic_viscosity=dynamic_viscosity,
+    )
