9b1cdf3426e9c3ac33455a7af59f42d5e21eb699
Kyle Gorkowski <kkgorkow@gmail.com>
2024-10-01 13:34:38 -0600

    fixed white space

--- particula/util/stats.py
+++ particula/util/stats.py
@@ -168,7 +168,7 @@ def average_to_interval(
             # interval, assumes that the time stream is sorted
             if start_index + time_lookup_span < len(time_raw):
                 compare_bool = np.nonzero(
-                    time_raw[start_index : start_index + time_lookup_span]
+                    time_raw[start_index: start_index + time_lookup_span]
                     >= time_i
                 )
                 if len(compare_bool[0]) > 0:
@@ -185,7 +185,7 @@ def average_to_interval(
                             * interval_look_buffer_multiple
                             / np.nanmean(
                                 np.diff(
-                                    time_raw[start_index : start_index + 100]
+                                    time_raw[start_index: start_index + 100]
                                 )
                             )
                         )
@@ -193,7 +193,7 @@ def average_to_interval(
                         time_lookup_span = 100
             else:
                 compare_bool = np.nonzero(
-                    time_raw[start_index : start_index + time_lookup_span]
+                    time_raw[start_index: start_index + time_lookup_span]
                     >= time_i
                 )
                 if len(compare_bool[0]) > 0:
