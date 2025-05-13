dab31c89fe567f09249a2705467864661081cb4f
Kyle Gorkowski <kkgorkow@gmail.com>
2025-05-12 19:22:03 -0600

    feat: add taichi backend package with initial imports and docstring

---
+++ particula/backend/taichi/__init__.py
@@ -0,0 +1,12 @@
+"""
+taichi backend for Particula
+
+import/initalize taichi
+"""
+
+import .dynamics
+import .particles
+import .particles.properties
+import .gas
+import .gas.properties
+import .util
