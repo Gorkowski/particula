86cbd2750dfa96f9c445ef21940183a5ccfbff7f
Kyle Gorkowski <kkgorkow@gmail.com>
2025-05-22 05:44:54 -0600

    fix: update function name and loop in particle radius calculation

--- particula/backend/taichi/particles/properties/ti_particle_radius.py
+++ particula/backend/taichi/particles/properties/ti_particle_radius.py
@@ -7,7 +7,7 @@ import taichi as ti
 
 
 @ti.func
-def get_particle_radius_via_masses(
+def fget_particle_radius_via_masses(
     particle_index: int,
     species_masses: ti.template(),
     density: ti.template(),
@@ -16,6 +16,6 @@ def get_particle_radius_via_masses(
     Calculate the radius of a particle based on its species masses and density.
     """
     volume = 0.0
-    for s in ti.ndrange(density.shape[0]):
+    for s in range(density.shape[0]):
         volume += species_masses[particle_index, s] / density[s]
     return ti.pow(3.0 * volume / (4.0 * ti.math.pi), 1.0 / 3.0)
